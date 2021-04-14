e017803eb56c99a2c6c1a550c7821ef42b9a62c1
Muhammad Zahid <muhammadali775@gmail.com>
2021-04-14 00:26:41 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let finishedCards = [];
+    for (let i=0; i < names.length; i++){
+    finishedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+    
+    }
+    return finishedCards;
+}
+
+
+function countDown(i){
+    while (i >= 0){
+    console.log(i)
+    i--
+
+    }
+}
+
+
